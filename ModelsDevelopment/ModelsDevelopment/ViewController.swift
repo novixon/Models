fe630c16648821d35ebaa6ba2c5892df1590d254
Gleb Novik <novik.gleb@gmail.com>
2017-06-14 22:29:05 +0300

    Added basic example (not working yet)

--- ModelsDevelopment/ModelsDevelopment/ViewController.swift
+++ ModelsDevelopment/ModelsDevelopment/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColoredTableViewCell: UITableViewCell, TableViewCell {
+    typealias Model = ColoredTableViewCellModel
+    
+    func setupWithModel(_ model: ColoredTableViewCellModel) {
+        backgroundColor = model.color
+    }
+}
 
+class ColoredTableViewCellModel: TableViewCellModel {
+    typealias Cell = ColoredTableViewCell
+    
+    let color: UIColor
+    
+    init(color: UIColor) {
+        self.color = color
+    }
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var cellFactory: TableViewCellFactory!
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cellFactory = TableViewCellFactory(tableView: mainTableView)
+        
+        mainTableView.delegate = self;
+        mainTableView.dataSource = self;
+        
+        mainTableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model: ColoredTableViewCellModel = ColoredTableViewCellModel(color: UIColor.green)
+        let cell = cellFactory.createCell(with: model)
+        
+        
+        return cell
+    }
 
 }
 
