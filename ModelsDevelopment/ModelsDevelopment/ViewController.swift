1b82cb6d8eee50c029f73640524a86f6b8e6d8cf
Gleb Novik <novik.gleb@gmail.com>
2017-06-14 17:41:59 +0300

    Added development project for the framework

---
+++ ModelsDevelopment/ModelsDevelopment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModelsDevelopment
+//
+//  Created by novik.home on 14.06.17.
+//  Copyright © 2017 Gleb Novik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
